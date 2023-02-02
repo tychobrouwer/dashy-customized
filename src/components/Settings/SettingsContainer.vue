68819b6bab31dc214451f9bdd904aa4f63a12191
TychoBrouwer <tychobrouwer33@gmail.com>
2023-02-02 17:06:43 +0100

    initial

--- src/components/Settings/SettingsContainer.vue
+++ src/components/Settings/SettingsContainer.vue
@@ -1,20 +1,18 @@
 <template>
   <section v-bind:class="{ 'settings-hidden': !settingsVisible }">
-    <SearchBar ref="SearchBar"
-      @user-is-searchin="userIsTypingSomething"
-      v-if="searchVisible"
-    />
+    <div class="options-outer" id="custom-added-container" data-v-0222a7c0></div>
+    <SearchBar ref="SearchBar" @user-is-searchin="userIsTypingSomething" v-if="searchVisible" />
     <div class="options-outer">
       <div :class="`options-container ${!settingsVisible ? 'hide' : ''}`">
         <ThemeSelector />
         <LayoutSelector :displayLayout="displayLayout" />
         <ItemSizeSelector :iconSize="iconSize" />
         <ConfigLauncher />
-        <AuthButtons  v-if="userState !== 0" :userType="userState" />
+        <AuthButtons v-if="userState !== 0" :userType="userState" />
       </div>
-      <div :class="`show-hide-container ${settingsVisible? 'hide-btn' : 'show-btn'}`">
-        <button @click="toggleSettingsVisibility()"
-          v-tooltip="`${settingsVisible? 'Hide' : 'Open'} Settings Menu`" tabindex="-2">
+      <div :class="`show-hide-container ${settingsVisible ? 'hide-btn' : 'show-btn'}`">
+        <button @click="toggleSettingsVisibility()" v-tooltip="`${settingsVisible ? 'Hide' : 'Open'} Settings Menu`"
+          tabindex="-2">
           <IconClose v-if="settingsVisible" />
           <IconOpen v-else />
         </button>
@@ -132,102 +130,119 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
 @import '@/styles/media-queries.scss';
 
-  section {
-    display: flex;
-    align-items: center;
-    align-items: stretch;
-    background: linear-gradient(0deg, var(--background) 0%, var(--background-darker) 100%);
-    box-shadow: var(--settings-container-shadow);
-  }
-  .options-outer {
-    display: flex;
-    position: relative;
-    flex: 1;
-    background: var(--settings-background);
-    border-radius: var(--curve-factor-navbar);
-  }
-  .options-container {
-    display: flex;
-    flex-direction: row;
-    align-items: flex-end;
-    justify-content: flex-end;
-    flex: 1;
-    padding: 0.5rem 1.5rem 0.5rem 1rem;
-    border-radius: var(--curve-factor-navbar) 0 0;
-    background: var(--settings-background);
-    div {
-      margin-left: 0.5rem;
-      opacity: var(--dimming-factor);
+section {
+  display: flex;
+  align-items: center;
+  align-items: stretch;
+  background: linear-gradient(0deg, var(--background) 0%, var(--background-darker) 100%);
+  box-shadow: var(--settings-container-shadow);
+}
+
+.options-outer {
+  display: flex;
+  position: relative;
+  flex: 1;
+  background: var(--settings-background);
+  border-radius: var(--curve-factor-navbar);
+}
+
+.options-container {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: flex-end;
+  flex: 1;
+  padding: 0.5rem 1.5rem 0.5rem 1rem;
+  border-radius: var(--curve-factor-navbar) 0 0;
+  background: var(--settings-background);
+
+  div {
+    margin-left: 0.5rem;
+    opacity: var(--dimming-factor);
+    opacity: 1;
+
+    &:hover {
       opacity: 1;
-      &:hover { opacity: 1; }
-    }
-    &.hide {
-      display: none;
-    }
-    @include very-tiny-phone {
-      flex-direction: column;
-      align-items: baseline;
-      div {
-        width: 100%;
-        text-align: center;
-        .theme-selector-section { justify-content: center; }
-      }
     }
   }
 
-  .show-hide-container {
-    display: flex;
-    // align-items: center;
-    background: var(--settings-background);
-    color: var(--settings-text-color);
-    width: 1.5rem;
-    position: absolute;
-    top: 4px;
-    right: 4px;
-    &.show-btn {
-      width: 2rem;
-      top: 0.5rem;
-      right: 0.5rem;
-      @include phone {
-        top: -3rem !important;
-      }
-    }
-    button {
+  &.hide {
+    display: none;
+  }
+
+  @include very-tiny-phone {
+    flex-direction: column;
+    align-items: baseline;
+
+    div {
       width: 100%;
-      padding: 2px 2px 0 2px;
-      margin: 2px;
-      border-radius: var(--curve-factor);
-      height: fit-content;
-      background: none;
-      border: none;
-      color: var(--settings-text-color);
-      cursor: pointer;
-      opacity: var(--dimming-factor);
-    }
-    &:hover button {
-      background: var(--settings-text-color);
-      color: var(--settings-background);
-    }
-  }
+      text-align: center;
 
-  @include tablet {
-    section {
-      display: block;
-      margin: 0 auto;
-      background: none;
-      .options-container {
-          justify-content: center;
+      .theme-selector-section {
+        justify-content: center;
       }
     }
   }
+}
 
-  @include phone {
-    .options-container, .show-hide-button {
-      // display: none;
+.show-hide-container {
+  display: flex;
+  // align-items: center;
+  background: var(--settings-background);
+  color: var(--settings-text-color);
+  width: 1.5rem;
+  position: absolute;
+  top: 4px;
+  right: 4px;
+
+  &.show-btn {
+    width: 2rem;
+    top: 0.5rem;
+    right: 0.5rem;
+
+    @include phone {
+      top: -3rem !important;
     }
   }
 
+  button {
+    width: 100%;
+    padding: 2px 2px 0 2px;
+    margin: 2px;
+    border-radius: var(--curve-factor);
+    height: fit-content;
+    background: none;
+    border: none;
+    color: var(--settings-text-color);
+    cursor: pointer;
+    opacity: var(--dimming-factor);
+  }
+
+  &:hover button {
+    background: var(--settings-text-color);
+    color: var(--settings-background);
+  }
+}
+
+@include tablet {
+  section {
+    display: block;
+    margin: 0 auto;
+    background: none;
+
+    .options-container {
+      justify-content: center;
+    }
+  }
+}
+
+@include phone {
+
+  .options-container,
+  .show-hide-button {
+    // display: none;
+  }
+}
 </style>
